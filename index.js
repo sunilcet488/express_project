3510613d38395bc8165230d24b6b51e0cc190a92
Sunil Kumar Das <sunilcet488@gmail.com>
2023-06-25 11:31:34 +0530

    express_project first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var jsonData = require("./vendorList.json");
+
+var express = require("express");
+// create an express microservice application
+var app = express();
+
+// listen to a port
+app.listen(3001);
+
+// middleware 
+
+app.use(express.json());
+
+// implement the end point microservice
+app.get("/", (req, res) => {
+  res.send("First Microservice GET Call");
+});
+
+// Get all the vendor
+app.get("/vendors", (req, res) => {
+  res.json(jsonData);
+});
+
+// filter vendor
+app.get("/vendors/:vendorId", (req, res) => {
+  
+  const vendorId = parseInt(req.params["vendorId"]);
+
+  const result = jsonData.filter(vendorData=> vendorData["vendorId"] == vendorId);
+  
+  res.json(result);
+});
+
+app.post("/vendors",(req,res)=>{
+    // read the body
+    var vendorData = req.body;
+    vendorData.vendorName = "Runtime Vendor Name";
+    res.json(vendorData);
+})
+
+console.log("Express webservice is running in port 3001");
